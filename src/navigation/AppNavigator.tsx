ae5712368d096854b92a732ae3a917effef2b211
daniel <danielbitmobb@gmail.com>
2025-05-23 17:44:37 -0500

    added AppNavigation and dependencies

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Catálogo' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
