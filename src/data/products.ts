dfa3e3312ab21c940f24dd31a1e92ca175a9fa92
daniel <danielbitmobb@gmail.com>
2025-05-29 13:36:48 -0500

    Added urls images unsplash

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,21 +3,70 @@ export const products = [
     id: '1',
     name: 'Vela aromática de vainilla',
     price: 45000,
-    description: 'Esta vela está hecha con ingredientes naturales y aromas suaves para crear una atmósfera relajante en tu hogar.',
-    image: 'https://images.unsplash.com/photo-1748228885250-49564b614db9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    description: 'Vela hecha con cera natural y esencia de vainilla para una atmósfera cálida y relajante.',
+    image: 'https://images.unsplash.com/photo-1720671847847-0b127f440aa3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
   {
     id: '2',
     name: 'Vela de lavanda relajante',
     price: 39000,
-    description: 'Esta vela está hecha con ingredientes naturales y aromas suaves para crear una atmósfera relajante en tu hogar.',
-    image: 'https://images.unsplash.com/photo-1748228885250-49564b614db9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    description: 'Ideal para dormir o meditar, con aceite esencial puro de lavanda.',
+    image: 'https://images.unsplash.com/photo-1617041261380-15cf80c39f26?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
   {
     id: '3',
     name: 'Vela de canela y naranja',
     price: 42000,
-    description: 'Esta vela está hecha con ingredientes naturales y aromas suaves para crear una atmósfera relajante en tu hogar.',
-    image: 'https://images.unsplash.com/photo-1748228885250-49564b614db9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    description: 'Fusión cálida de canela y naranja, perfecta para tardes de otoño.',
+    image: 'https://images.unsplash.com/photo-1602607203559-d38903b80507?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '4',
+    name: 'Vela de eucalipto fresco',
+    price: 40000,
+    description: 'Revitalizante aroma de eucalipto ideal para baños y momentos de spa.',
+    image: 'https://images.unsplash.com/photo-1745700812572-cc7af3acbc68?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '5',
+    name: 'Vela de coco tropical',
+    price: 43000,
+    description: 'Aroma dulce y suave del coco, ideal para espacios relajados.',
+    image: 'https://images.unsplash.com/photo-1720671847847-0b127f440aa3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '6',
+    name: 'Vela de rosas silvestres',
+    price: 46000,
+    description: 'Romántico aroma floral para momentos íntimos o decoraciones especiales.',
+    image: 'https://images.unsplash.com/photo-1745125996166-aa81a27d4653?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '7',
+    name: 'Vela de menta y albahaca',
+    price: 41000,
+    description: 'Fragancia fresca y herbal para energizar cualquier ambiente.',
+    image: 'https://images.unsplash.com/photo-1745284053491-80beb8f33ef7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '8',
+    name: 'Vela de jazmín nocturno',
+    price: 47000,
+    description: 'Notas florales y dulces que evocan una noche tranquila bajo las estrellas.',
+    image: 'https://images.unsplash.com/photo-1720671847847-0b127f440aa3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '9',
+    name: 'Vela de frutos rojos',
+    price: 44000,
+    description: 'Aroma dulce y vibrante que recuerda a frutas del bosque.',
+    image: 'https://images.unsplash.com/photo-1617041261380-15cf80c39f26?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '10',
+    name: 'Vela de madera de cedro',
+    price: 48000,
+    description: 'Aroma cálido, amaderado y sofisticado. Perfecta para oficinas y salas de estar.',
+    image: 'https://images.unsplash.com/photo-1745700812572-cc7af3acbc68?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
 ];
